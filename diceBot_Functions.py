39529658b9c9207fabd550ceecd07da75ae7aaf6
Snak3Doc <benjaminbadham@gmail.com>
2022-08-18 12:51:26 +0700

    Updated: Minor string refactoring

--- diceBot_Functions.py
+++ diceBot_Functions.py
@@ -70,7 +70,7 @@ async def diceFunction(message):
     #?---Final Results---
     diceInput = diceInput.replace(':', ' : ')
     emb.color = Color.orange()
-    emb.description = f"**Total Throw Value: {throwTotal}\nThrow Operator: {throwTotal} {operator} {operatorNum}\nFinal Throw Value: {operatorAns}**"
+    emb.description = f"**Total Throw Value:** {throwTotal}\n**Throw Operator:** {throwTotal} {operator} {operatorNum}\n**Final Throw Value:** {operatorAns}"
     #emb = Embed(description= f"Total Throw Value: {throwTotal}\nThrow Operator: {throwTotal} {operator} {operatorNum}\nFinal Throw Value: {operatorAns}") 
     emb.set_author(name = message.author.display_name + f" Rolled: {diceInput}", icon_url = message.author.avatar_url)
     await message.channel.send(embed=emb)
